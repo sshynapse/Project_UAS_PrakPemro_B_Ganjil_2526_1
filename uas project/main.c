4da1b389556809133447a47470f49615d90d552b
rafiealghifari987-cpu <rafiealghifari987@gmail.com>
2025-11-16 11:34:32 +0700

    Set up main function and load initial data
    Initialize main function with login and data loading.

--- uas project/main.c
+++ uas project/main.c
@@ -1,1 +1,17 @@
+#include "perpustakaan.h"
+
+int main() {
+    if (!login()) {
+        printf("Login gagal. Program berhenti.\n");
+        return 1;
+    }
+
+    Buku *books = NULL;
+    Anggota *members = NULL;
+    Peminjaman *loans = NULL;
+    int book_count = 0, member_count = 0, loan_count = 0;
+
+    load_books(&books, &book_count);
+    load_members(&members, &member_count);
+    load_loans(&loans, &loan_count);
 
